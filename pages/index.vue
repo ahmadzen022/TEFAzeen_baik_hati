f098799b5ad3c5c0fcc2e9bf8cfa47b64de7a098
ahmadzen022 <141709205+ahmadzen022@users.noreply.github.com>
2024-06-06 14:13:52 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -125,7 +125,7 @@
    
   }
   .container-fluid {
-    background-image: url("@/assets/img/bg.jpg");
+    background-image: url("@/assets/img/bg.webp");
     background-size:cover;
     width: 100%;
     height: 100vh;
